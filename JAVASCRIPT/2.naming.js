07bdf201637e42fba6e1c64552573df3d7bccfb9
JaeWooYi <wodn0101@gmail.com>
2022-04-15 21:38:35 +0900

    Friday

---
+++ JAVASCRIPT/2.naming.js
@@ -0,0 +1,14 @@
+/** 변수 규칙
+ *  라틴문자(0~9, 소문자, 대문자), _
+ *  대소문자 구분
+ *  camelCase쓰기
+ *  (에약어 , 숫자로 시작, 특수문자 시작) x
+ */
+
+// ex)
+let apple = "apple";
+let redApple = "Red Apple!";
+
+console.log(apple);
+console.log(redApple);
+
